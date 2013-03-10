a6075b4e042dd5290e1c6275515ad5f155c08279
Olivier Laviale <contact@weirdog.com>
2013-03-10 10:32:07 +0100

    ICanBoogie update

--- config/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return array
-(
-	'BlueTihi\Context' => $path . (version_compare(PHP_VERSION, '5.3.4') < 0 ? 'lib/context_compat.php' : 'lib/context.php'),
-	'BlueTihi\Engine' => $path . 'lib/core.php'
-);
